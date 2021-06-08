3e5a3af98940107a9063d4ac203c898c842d47b9
chickenishere <aaronmullen1810@gmail.com>
2021-06-08 19:44:31 +0200

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -105,9 +105,9 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 	charizard: {
 		num: 6,
 		name: "Charizard",
-		types: ["Fire", "Flying"],
+		types: ["Rock", "Flying"],
 		genderRatio: {M: 0.875, F: 0.125},
-		baseStats: {hp: 78, atk: 84, def: 78, spa: 109, spd: 85, spe: 100},
+		baseStats: {hp: 78, atk: 120, def: 78, spa: 109, spd: 85, spe: 100},
 		abilities: {0: "Blaze", H: "Solar Power"},
 		heightm: 1.7,
 		weightkg: 90.5,
